eb0765a9519365c2641dcaaf1e7b0d81446c4fd5
Victor Silva <58794491+victorl1m@users.noreply.github.com>
2022-11-25 15:27:41 -0300

    new api

--- components/AppInfo/AppInfo.jsx
+++ components/AppInfo/AppInfo.jsx
@@ -23,7 +23,7 @@ export default function AppInfo() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    fetch("https://apiPubnews.vitaoks1.repl.co")
+    fetch("https://APIQuarkzPlace.vitaoks1.repl.co")
       .then((response) => response.json())
       .then((data) => setData(data));
   }, []);
