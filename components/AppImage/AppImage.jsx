21c76b9f41456a1dc1e7c36729f671c9dea7e769
Victor Silva <58794491+victorl1m@users.noreply.github.com>
2022-11-25 15:27:57 -0300

    new api

--- components/AppImage/AppImage.jsx
+++ components/AppImage/AppImage.jsx
@@ -6,7 +6,7 @@ export default function AppImage() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    fetch("https://apiPubnews.vitaoks1.repl.co")
+    fetch("https://APIQuarkzPlace.vitaoks1.repl.co")
       .then((response) => response.json())
       .then((data) => setData(data));
   }, []);
