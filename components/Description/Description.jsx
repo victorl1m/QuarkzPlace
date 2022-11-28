2f3e8683d57fbc6d04158a2277e0cc6d34285845
Marcos Segantine <msegantine8@gmail.com>
2022-11-28 20:53:05 -0300

    fixed description ortography

--- components/Description/Description.jsx
+++ components/Description/Description.jsx
@@ -6,10 +6,9 @@ export default function Description() {
     <div className="desc-container">
       <h1 className="description-title">Descrição do aplicativo</h1>
       <p className="description-text">
-        No SIGO você poderá gerenciar suas obras com facilidade: as informações
-        de clientes, da equipe de trabalho, dos prestadores de serviço e
-        fornecedores ficam disponíveis de modo prático e rápido, tanto gestores
-        quanto clientes têm acesso ao andamento da obra na palma da mão!
+        No SIGO você poderá gerenciar suas obras com mais facilidade, todas as informações
+        de seus clientes, de sua equipe de trabalho, dos prestadores de serviço e
+        juntamente da informação de seus fornecedores que ficam disponíveis de modo prático e rápido.
       </p>
     </div>
   );
